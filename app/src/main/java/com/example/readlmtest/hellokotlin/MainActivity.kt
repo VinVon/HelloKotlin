03034a8c86e7e0d53eb55f67a52fbe6a4110afac
VinVon <642751580@qq.com>
2017-07-14 10:47:34 +0800

    CHANGE

--- app/src/main/java/com/example/readlmtest/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/readlmtest/hellokotlin/MainActivity.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var str="dsa"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
